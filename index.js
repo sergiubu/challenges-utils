4a0cf98953e23ef58037b123e4588f7e61aa9ec2
Sergiu <sil3ent@yahoo.com>
2020-03-27 16:47:55 +0200

    Add ES6 Destructuring Objects challenge

--- index.js
+++ index.js
@@ -125,3 +125,20 @@ function oneOddOneEven(n) {
 }
 
 console.log(oneOddOneEven(26));
+
+/* 
+The code below produces an error because
+no object was passed to the function.
+Fix the function to return the default size,
+even if nothing is passed to the function.
+Don't remove the {size = "big"} object in the
+parameter and don't change the return statement.
+*/
+function shirtSize({ size = 'big' }) {
+  return size;
+}
+// Solution
+// function ({size} = {size: "big"})
+function shirtSize({ size = 'big' } = {}) {
+  return size;
+}
