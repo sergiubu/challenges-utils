16658cba429983d2e5cdf8574ed0d6f9ffe310c5
Sergiu <sil3ent@yahoo.com>
2020-04-15 15:27:18 +0300

    Add Remove trailing and leading zeros challenge

--- index.js
+++ index.js
@@ -370,3 +370,32 @@ console.log(this.planetname);
 // this goes to global window
 var output = obj.prop.getFullname;
 console.log(output());
+
+/* 
+Create a function that takes in a number as a
+string n and returns the number without trailing
+and leading zeros.
+
+- Trailing Zeros are the zeros after a decimal
+point which don't affect the value
+(e.g. the last three zeros in 3.4000 and 3.04000).
+- Leading Zeros are the zeros before a whole
+number which don't affect the value
+(e.g. the first three zeros in 000234 and 000230).
+- Return a string.
+- If you get a number with .0 on the end,
+return the integer value
+(e.g. return "4" rather than "4.0").
+- If the number is 0, 0.0, 000, 00.00, etc... return "0".
+*/
+function removeLeadingTrailing(n) {
+  return String(+n);
+
+  // return Math.abs(n);
+  // return  parseFloat(n);
+
+  // return +(n.match(/[1-9]\d*|(?=\.)\.\d*[1-9]/g) || []).join('');
+}
+
+console.log(removeLeadingTrailing('230.000'));
+console.log(removeLeadingTrailing('03.1400'));
