4310afd299b638ae6ff07b2713f2af230cce1c25
Sergiu B <sil3ent@yahoo.com>
2020-07-16 13:54:31 +0300

    Add Anonymous Functions: Add Suffix Challenge

--- index.js
+++ index.js
@@ -627,3 +627,13 @@ function sliceSum(arr, n) {
 }
 
 console.log(sliceSum([0, 0, 0, 3, 4], 3));
+
+// All About Anonymous Functions: Adding Suffixes
+// Write a function that returns an anonymous function, which transforms its input by adding a particular suffix at the end.
+function add_suffix(suffix) {
+  return (word) => word + suffix;
+}
+
+add_ly = add_suffix('ly');
+
+console.log(add_ly('hopeless'));
