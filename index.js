c9efe67b47809f938dd39be3eedf30312ba950cf
Sergiu B <sil3ent@yahoo.com>
2020-08-19 15:26:43 +0300

    Add Number of Boomerangs Challenge

--- index.js
+++ index.js
@@ -710,3 +710,53 @@ console.log(convert('100°C'));
 console.log(convert('0°F'));
 console.log(convert('33'));
 console.log(convert('7K'));
+
+// Number of Boomerangs
+// A boomerang is a V-shaped sequence that is either upright or upside down. Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different.
+// Some boomerang examples:
+// [3, 7, 3], [1, -1, 1], [5, 6, 5]
+// Create a function that returns the total number of boomerangs in an array.
+// To illustrate:
+// [3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2]
+// 3 boomerangs in this sequence:  [3, 7, 3], [1, 5, 1], [2, -2, 2]
+// Be aware that boomerangs can overlap, like so:
+// [1, 7, 1, 7, 1, 7, 1]
+// 5 boomerangs (from left to right): [1, 7, 1], [7, 1, 7], [1, 7, 1], [7, 1, 7], and [1, 7, 1]
+// Notes
+// [5, 5, 5] (triple identical digits) is NOT considered a boomerang because the middle digit is identical to the first and last.
+function countBoomerangs(arr) {
+  return arr.reduce(
+    (acc, e, index) => acc + (e === arr[index + 2] && e != arr[index + 1]),
+    0
+  );
+
+  // return arr.filter((_, i) => arr[i] == arr[i + 2] && arr[i + 1] != arr[i])
+  //   .length;
+
+  // return arr.slice(2).reduce((total, last, i) => {
+  //   const [first, middle] = [arr[i], arr[i + 1]];
+  //   return total + Number(first === last && first !== middle);
+  // }, 0);
+
+  // let stack = [];
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] === arr[i + 2] && arr[i] !== arr[i + 1]) {
+  //     stack.push(arr.slice(i, i + 3));
+  //   }
+  // }
+  // return stack.length;
+
+  // const res = [];
+
+  // arr.forEach((val, i) => {
+  //   if (val === arr[i + 2] && val !== arr[i + 1]) {
+  //     res.push([val, arr[i + 1], arr[i + 2]]);
+  //   }
+  // });
+
+  // return res;
+  // return res.length;
+}
+
+console.log(countBoomerangs([9, 5, 9, 5, 1, 1, 1]));
+console.log(countBoomerangs([1, 7, 1, 7, 1, 7, 1]));
