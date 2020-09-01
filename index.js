6c37b58262edddcc2887a60474ff02251c76dd1b
Sergiu B <sil3ent@yahoo.com>
2020-09-01 13:18:20 +0300

    Add Find Value in a Binary Tree Challenge

--- index.js
+++ index.js
@@ -965,3 +965,49 @@ function paths(n) {
 }
 
 console.log(paths(5));
+
+// Find Value in a Binary Tree
+// An array that represents a Binary Tree is in the following form:
+
+// binaryTree = [val, arrLeft, arrRight]
+
+// When arrLeft is the left side of the tree and arrRight is the right side of the tree.
+
+// To illustrate:
+// const arr1 = [3, [ 8, [ 5, null, null], null], [ 7, null, null]]
+
+// arr1 represents the following Binary Tree:
+
+//       3
+//      / \
+//     8   7
+//    /\   /\
+//   5  N N  N
+//  /\
+//  N N
+
+// Where N represents null.
+
+// Create a function that takes an array that represent a Binary Tree and a value and return true if the value is in the tree and, false otherwise.
+//  - The tree will contain integers only and will be presented by an array in the specified format.
+function valueInTree(tree, val) {
+  return tree
+    ? tree[0] === val || valueInTree(tree[1], val) || valueInTree(tree[2], val)
+    : false;
+
+  // return Boolean(
+  //   tree
+  //     .toString()
+  //     .split(',')
+  //     .find((v) => +v === val)
+  // );
+
+  // return tree.flat(Infinity).includes(val);
+}
+
+console.log(
+  valueInTree(
+    [3, [7, [1, null, null], [8, null, null]], [5, null, [4, null, null]]],
+    7
+  )
+);
